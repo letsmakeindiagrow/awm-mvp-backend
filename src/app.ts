a1d22603dd17979c9e6754f2f7beceb0585d2ef3
pushkar1713 <pushkar1713@gmail.com>
2025-04-16 15:45:43 +0530

    updated cors with  credentials

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,25 @@ import investor from "./routes/investor.routes.js";
 
 dotenv.config();
 
+const allowedOrigins = [
+  "https://login.aadyanviwealth.com",
+  "https://investor.aadyanviwealth.com",
+  "https://www.aadyanviwealth.com",
+];
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
